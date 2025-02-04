46377251e8665f3dabeb4cd62a490c0cad72e695
Pablo Matheo Flores Loor <105876330+Esqueleto27@users.noreply.github.com>
2025-02-04 13:29:25 -0500

    Delete Repaso/Ejercicios directory

--- Repaso/Ejercicios/2ejercicio.py
+++
@@ -1,6 +0,0 @@
-# Tabla de multiplicar
-
-numero = int(input("Ingrese su numero: "))
-for num in range(1, 11):
-    resulatado = num * numero
-    print(f"El numero {numero} X {num} es igual a: {resulatado}")
--- Repaso/Ejercicios/3ejercicio.py
+++
@@ -1,4 +0,0 @@
-# numeros pares
-
-for numero in range(2, 101, 2):
-    print(numero)
