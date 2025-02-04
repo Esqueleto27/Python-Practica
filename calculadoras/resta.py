b1aaf556e509bf9e7402a57cce904a21a5a9c5e7
Esqueleto27 <matheofloresloor@gmail.com>
2025-02-04 15:58:41 -0500

    cambio de numeros a letras

--- calculadoras/resta.py
+++ calculadoras/resta.py
@@ -1,7 +1,7 @@
 #Resta 
 
-n1 = int(input("Ingrese su 1er numero: "))
-n2 = int(input("Ingrese su 2do numero: "))
+n1 = int(input("Ingrese su primer numero: "))
+n2 = int(input("Ingrese su segundo numero: "))
 mensaje = n1 + n2
 print("Su resultado es de: ", mensaje)
 #Esto es para restar
