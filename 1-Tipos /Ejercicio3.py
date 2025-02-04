7636a09d41d1661bf8a4f0fbc9d805a10ed6a9d1
Pablo Matheo Flores Loor <105876330+Esqueleto27@users.noreply.github.com>
2025-02-04 13:28:50 -0500

    Delete 1-Tipos  directory

--- 1-Tipos /Ejercicio3.py
+++
@@ -1,9 +0,0 @@
-# Calculadora de cambio
-
-costo_producto = int(input("Cuanto valio el producto: "))
-cantidad_dinero_entregada = int(input("Cuanto dinero entrego el cliente: "))
-cambio = cantidad_dinero_entregada - costo_producto
-if cantidad_dinero_entregada < costo_producto:
-    print("Falta dinero")
-else:
-    print(f"El cambio es de: {cambio}")
